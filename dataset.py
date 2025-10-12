39f190237bf97b0112086a5c04893f633f235ecf
abmahmoud03 <abmahmoud03@gmail.com>
2025-10-12 14:16:23 -0400

    remove prints and update resize

--- dataset.py
+++ dataset.py
@@ -5,6 +5,7 @@ import os
 import random
 import cv2 as cv
 from torchvision import datasets, transforms
+import skimage
 from stride_augmentation import *
 from checkboard_augmentation import *
 
@@ -21,7 +22,7 @@ class DatasetFromFolder(data.Dataset):
         print(self.input_path)
         print(self.target_path)
         self.image_filenames = [x for x in sorted(os.listdir(self.input_path))]
-        print(self.image_filenames)
+        # print(self.image_filenames)
         self.direction = direction
         self.transform = transform
         self.resize_scale = resize_scale    # resize_scale = 286
@@ -32,10 +33,12 @@ class DatasetFromFolder(data.Dataset):
         # Load Image
         img_fn = os.path.join(self.input_path, self.image_filenames[index])
         img_tar = os.path.join(self.target_path, self.image_filenames[index])
-#         print("img_fn is {}".format(img_fn))
-#         print("img_tar is {}".format(img_tar))
+        # print("img_fn is {}".format(img_fn))
+        # print("img_tar is {}".format(img_tar))
         img_input = cv.imread(img_fn)
-        img_target = cv.imread(img_tar)
+        
+        # Replaced with uncorrupted image
+        img_target = cv.imread(img_fn)
         
         stride = False
         
@@ -46,15 +49,22 @@ class DatasetFromFolder(data.Dataset):
         else:
             # checkboard augmentation
             # mask1 = random_checkboard_mask(img_input, None)
-            mask1 = random_checkboard_mask_new(img_input, None)
-    
+            mask1 = random_checkboard_mask(None)
+
+
         img_input = img_input * mask1
         
         
         # preprocessing
         if self.resize_scale:
-            img_input = cv.resize(img_input, (self.resize_scale, self.resize_scale))
-            img_target = cv.resize(img_target, (self.resize_scale, self.resize_scale))
+            img_input = skimage.transform.resize(
+                img_input, 
+                (self.resize_scale, self.resize_scale)
+            )
+            img_target = skimage.transform.resize(
+                img_target, 
+                (self.resize_scale, self.resize_scale)
+            )
 #             print("img_target.size is {}".format(img_target.shape))
 
         if self.crop_size:
